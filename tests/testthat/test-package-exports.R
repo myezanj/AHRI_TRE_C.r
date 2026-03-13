fa5355eb447c3e2da80909d43c2e7d5bbe0c86a4
Myeza <34543679+myezanj@users.noreply.github.com>
2026-03-13 11:28:19 +0200

    Update test-package-exports.R

--- tests/testthat/test-package-exports.R
+++ tests/testthat/test-package-exports.R
@@ -1,4 +1,4 @@
 test_that("core exported functions exist", {
-  expect_true(exists("ahri_tre_load", where = asNamespace("AHRITREC"), inherits = FALSE))
-  expect_true(exists("ahri_tre_version", where = asNamespace("AHRITREC"), inherits = FALSE))
+  expect_true(exists("tre_load", where = asNamespace("AHRITREC"), inherits = FALSE))
+  expect_true(exists("version", where = asNamespace("AHRITREC"), inherits = FALSE))
 })
