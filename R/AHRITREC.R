134e33c16ba085dc362069ebc846ca79f75fcb9d
Myeza <34543679+myezanj@users.noreply.github.com>
2026-03-13 11:28:38 +0200

    Update AHRITREC.R

--- R/AHRITREC.R
+++ R/AHRITREC.R
@@ -1,15 +1,46 @@
-ahri_tre_load <- function(core_path = Sys.getenv("AHRI_TRE_C_LIB", unset = "")) {
-  if (!nzchar(core_path)) {
-    staged_core_dir <- system.file("ahri_tre_core", package = "AHRITREC")
-    if (nzchar(staged_core_dir)) {
-      if (.Platform$OS.type == "windows") {
-        staged_candidates <- file.path(staged_core_dir, c("ahri_tre_c.dll", "libahri_tre_c.dll"))
-      } else if (Sys.info()[["sysname"]] == "Darwin") {
-        staged_candidates <- file.path(staged_core_dir, "libahri_tre_c.dylib")
-      } else {
-        staged_candidates <- file.path(staged_core_dir, "libahri_tre_c.so")
-      }
+.tre_sysname <- function() {
+  Sys.info()[["sysname"]]
+}
 
+.tre_lib_names <- function() {
+  if (.Platform$OS.type == "windows") {
+    return(c("tre_c.dll", "libtre_c.dll", "ahri_tre_c.dll", "libahri_tre_c.dll"))
+  }
+  if (.tre_sysname() == "Darwin") {
+    return(c("libtre_c.dylib", "libahri_tre_c.dylib"))
+  }
+  c("libtre_c.so", "libahri_tre_c.so")
+}
+
+.tre_build_candidates <- function(root) {
+  lib_names <- .tre_lib_names()
+  c(
+    file.path(root, "c_core", "build", lib_names),
+    file.path(root, "c_core", "build", "Release", lib_names)
+  )
+}
+
+.tre_call_chr1 <- function(symbol, arg1) {
+  .Call(symbol, as.character(arg1))
+}
+
+.tre_call_chr2 <- function(symbol, arg1, arg2) {
+  .Call(symbol, as.character(arg1), as.character(arg2))
+}
+
+.tre_call_chr_nullable2 <- function(symbol, arg1, arg2 = NULL) {
+  if (is.null(arg2)) {
+    .Call(symbol, as.character(arg1), NULL)
+  } else {
+    .Call(symbol, as.character(arg1), as.character(arg2))
+  }
+}
+
+tre_load <- function(core_path = Sys.getenv("AHRI_TRE_C_LIB", unset = "")) {
+  if (!nzchar(core_path)) {
+    staged_core_dir <- system.file("tre_core", package = "AHRITREC")
+    if (nzchar(staged_core_dir)) {
+      staged_candidates <- file.path(staged_core_dir, .tre_lib_names())
       staged_found <- staged_candidates[file.exists(staged_candidates)]
       if (length(staged_found) > 0) {
         core_path <- staged_found[[1]]
@@ -28,8 +59,7 @@ ahri_tre_load <- function(core_path = Sys.getenv("AHRI_TRE_C_LIB", unset = "")) 
     sibling_names <- c("AHRI_TRE.C", "AHRI_TRE.c", "AHRI_TRE.jl")
     repeat {
       parent <- dirname(anchor)
-      sibling_paths <- file.path(parent, sibling_names)
-      roots <- c(roots, normalizePath(sibling_paths, mustWork = FALSE))
+      roots <- c(roots, normalizePath(file.path(parent, sibling_names), mustWork = FALSE))
       if (parent == anchor) {
         break
       }
@@ -37,29 +67,7 @@ ahri_tre_load <- function(core_path = Sys.getenv("AHRI_TRE_C_LIB", unset = "")) 
     }
 
     roots <- unique(roots)
-    candidates <- character(0)
-    for (root in roots) {
-      if (.Platform$OS.type == "windows") {
-        candidates <- c(
-          candidates,
-          file.path(root, "c_core", "build", "Release", "ahri_tre_c.dll"),
-          file.path(root, "c_core", "build", "ahri_tre_c.dll")
-        )
-      } else if (Sys.info()[["sysname"]] == "Darwin") {
-        candidates <- c(
-          candidates,
-          file.path(root, "c_core", "build", "libahri_tre_c.dylib"),
-          file.path(root, "c_core", "build", "Release", "libahri_tre_c.dylib")
-        )
-      } else {
-        candidates <- c(
-          candidates,
-          file.path(root, "c_core", "build", "libahri_tre_c.so"),
-          file.path(root, "c_core", "build", "Release", "libahri_tre_c.so")
-        )
-      }
-    }
-
+    candidates <- unlist(lapply(roots, .tre_build_candidates), use.names = FALSE)
     found <- candidates[file.exists(candidates)]
     if (length(found) > 0) {
       core_path <- found[[1]]
@@ -74,82 +82,44 @@ ahri_tre_load <- function(core_path = Sys.getenv("AHRI_TRE_C_LIB", unset = "")) 
   stop("Could not locate AHRI TRE C shared library. Reinstall package or set AHRI_TRE_C_LIB.")
 }
 
-ahri_tre_version <- function() {
-  .Call("ahri_tre_version_R")
+version <- function() .Call("version_R")
+
+sha256_file_hex <- function(path) .tre_call_chr1("sha256_file_hex_R", path)
+
+verify_sha256_file <- function(path, expected_hex) {
+  .tre_call_chr2("verify_sha256_file_R", path, expected_hex)
 }
 
-ahri_tre_sha256_file_hex <- function(path) {
-  .Call("ahri_tre_sha256_file_hex_R", as.character(path))
-}
+parse_flavour <- function(flavour) .tre_call_chr1("parse_flavour_R", flavour)
 
-ahri_tre_verify_sha256_file <- function(path, expected_hex) {
-  .Call("ahri_tre_verify_sha256_file_R", as.character(path), as.character(expected_hex))
-}
+map_sql_type_to_tre <- function(sql_type) .tre_call_chr1("map_sql_type_to_tre_R", sql_type)
 
-ahri_tre_parse_flavour <- function(flavour) {
-  .Call("ahri_tre_parse_flavour_R", as.character(flavour))
-}
+extract_table_from_sql <- function(sql) .tre_call_chr1("extract_table_from_sql_R", sql)
 
-ahri_tre_map_sql_type_to_tre <- function(sql_type) {
-  .Call("ahri_tre_map_sql_type_to_tre_R", as.character(sql_type))
-}
+parse_in_list_values_json <- function(values) .tre_call_chr1("parse_in_list_values_json_R", values)
+parse_in_list_values <- function(values) parse_in_list_values_json(values)
 
-ahri_tre_extract_table_from_sql <- function(sql) {
-  .Call("ahri_tre_extract_table_from_sql_R", as.character(sql))
+parse_check_constraint_values_json <- function(constraint_def, column_name) {
+  .tre_call_chr2("parse_check_constraint_values_json_R", constraint_def, column_name)
 }
-
-ahri_tre_parse_in_list_values_json <- function(values) {
-  .Call("ahri_tre_parse_in_list_values_json_R", as.character(values))
-}
-
-parse_in_list_values <- function(values) {
-  ahri_tre_parse_in_list_values_json(values)
-}
-
-ahri_tre_parse_check_constraint_values_json <- function(constraint_def, column_name) {
-  .Call("ahri_tre_parse_check_constraint_values_json_R", as.character(constraint_def), as.character(column_name))
-}
-
 parse_check_constraint_values <- function(constraint_def, column_name) {
-  ahri_tre_parse_check_constraint_values_json(constraint_def, column_name)
-}
-
-ahri_tre_map_redcap_value_type <- function(field_type, validation = NULL) {
-  if (is.null(validation)) {
-    .Call("ahri_tre_map_redcap_value_type_R", as.character(field_type), NULL)
-  } else {
-    .Call("ahri_tre_map_redcap_value_type_R", as.character(field_type), as.character(validation))
-  }
+  parse_check_constraint_values_json(constraint_def, column_name)
 }
 
 map_value_type <- function(field_type, validation = NULL) {
-  ahri_tre_map_redcap_value_type(field_type, validation)
+  .tre_call_chr_nullable2("map_value_type_R", field_type, validation)
 }
 
-ahri_tre_parse_redcap_choices_json <- function(choices) {
-  .Call("ahri_tre_parse_redcap_choices_json_R", as.character(choices))
-}
+parse_redcap_choices_json <- function(choices) .tre_call_chr1("parse_redcap_choices_json_R", choices)
+parse_redcap_choices <- function(choices) parse_redcap_choices_json(choices)
 
-parse_redcap_choices <- function(choices) {
-  ahri_tre_parse_redcap_choices_json(choices)
-}
+strip_html <- function(text) .tre_call_chr1("strip_html_R", text)
 
-ahri_tre_strip_html <- function(text) {
-  .Call("ahri_tre_strip_html_R", as.character(text))
-}
+infer_label_from_field_name <- function(field_name) .tre_call_chr1("infer_label_from_field_name_R", field_name)
 
-ahri_tre_infer_label_from_field_name <- function(field_name) {
-  .Call("ahri_tre_infer_label_from_field_name_R", as.character(field_name))
+get_redcap_choices_for_field_json <- function(field_type, choices = NULL) {
+  .tre_call_chr_nullable2("get_redcap_choices_for_field_json_R", field_type, choices)
 }
-
-ahri_tre_get_redcap_choices_for_field_json <- function(field_type, choices = NULL) {
-  if (is.null(choices)) {
-    .Call("ahri_tre_get_redcap_choices_for_field_json_R", as.character(field_type), NULL)
-  } else {
-    .Call("ahri_tre_get_redcap_choices_for_field_json_R", as.character(field_type), as.character(choices))
-  }
-}
-
 get_redcap_choices_for_field <- function(field_type, choices = NULL) {
-  ahri_tre_get_redcap_choices_for_field_json(field_type, choices)
+  get_redcap_choices_for_field_json(field_type, choices)
 }
